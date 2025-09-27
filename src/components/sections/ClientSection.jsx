a4f974fe1257fab9e4552fa99fde4b05d10f05a9
Thesujeetkr <sksujeetkumar99@gmail.com>
2025-09-27 15:57:58 +0530

    fixed

--- src/components/sections/ClientSection.jsx
+++ src/components/sections/ClientSection.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 // import clientBg from "../../assets/clients.png";
-import clientBg from "../../assets/clients.png";
+import clientBg from "../../assets/Clients.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
